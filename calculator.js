9b0b5a97d8fe176b8f39be29aa5226971cc64d01
Alois Sečkár <ellrohir@seznam.cz>
2023-10-14 23:20:17 +0200

    reset input after evaluation
    pressing next number after "=" will reset the input, pressing operation will preserve the result as first operand

--- calculator.js
+++ calculator.js
@@ -2,28 +2,35 @@ let input = document.getElementById('inputbox');
 let buttons = document.querySelectorAll('button');
 
 let string = "";
+let evaluated = false;
 let arr = Array.from(buttons);
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
         if (e.target.innerHTML == '=') {
             string = eval(string);
             input.value = string;
+            evaluated = true;
         }
 
         else if (e.target.innerHTML == 'AC') {
             string = "";
             input.value = string;
+            evaluated = false;
         }
 
         else if (e.target.innerHTML == 'DEL') {
             string = string.substring(0, string.length - 1);
             input.value = string;
+            evaluated = false;
         }
 
-
         else {
+			if (evaluated && !isNaN(e.target.innerHTML)) {
+				string = "";
+			}
             string += e.target.innerHTML;
             input.value = string;
+            evaluated = false;
         }
 
     })
